be1a7ee561254306a6244d78cf02fd0182c1c4d7
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2026-04-26 20:07:51 +0100

    almost

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 09:57:45 by htrindad          #+#    #+#             */
-/*   Updated: 2026/04/26 19:42:59 by htrindad         ###   ########.fr       */
+/*   Updated: 2026/04/26 20:06:26 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ const char *BitcoinExchange::InvalidFile::what() const throw() { return "Invalid
 static inline std::map<std::string, float> fileParser(std::ifstream &file)
 {
 	std::map<std::string, float>	m;
-	std::istringstream		ss;
 	std::string			line;
 	std::string			date;
 	std::string			btc;
@@ -29,10 +28,10 @@ static inline std::map<std::string, float> fileParser(std::ifstream &file)
 	std::getline(file, line); // skip "data,exchange_rate"
 	while (std::getline(file, line))
 	{
-		ss << line;
+		std::istringstream ss(line);
 		std::getline(ss, date, ',');
 		std::getline(ss, btc);
-		std::ifstringstream(btc) >> value;
+		std::istringstream(btc) >> value;
 		m.insert(std::make_pair(date, value));
 	}
 	return m;
@@ -41,20 +40,20 @@ static inline std::map<std::string, float> fileParser(std::ifstream &file)
 static inline void thrower(std::ifstream &stream)
 {
 	if (stream.fail())
-		throw InvalidFile();
+		throw BitcoinExchange::InvalidFile();
 	if (!stream.is_open())
-		throw FileNotOpen();
+		throw BitcoinExchange::FileNotOpen();
 }
 
-static inline void finders(const std::map<std::string, float> &m, const std::ifstream &f)
+static inline void finders(const std::map<std::string, float> &m, std::ifstream &f)
 {
-	std::map<std::string, float>::iterator	it;
-	std::string				line;
-	std::string				date;
-	std::size_t				fo;
-	std::size_t				start;
-	std::size_t				end;
-	float					btc;
+	std::map<std::string, float>::const_iterator	it;
+	std::string					line;
+	std::string					date;
+	std::size_t					fo;
+	std::size_t					start;
+	std::size_t					end;
+	float						btc;
 
 	it = m.begin();
 	std::getline(f, line);
@@ -62,7 +61,7 @@ static inline void finders(const std::map<std::string, float> &m, const std::ifs
 	{
 		fo = line.find_first_of("|");
 		date = line.substr(0, fo);
-		std::ifstringstream(line.substr(fo + 1)) >> btc;
+		std::istringstream(line.substr(fo + 1)) >> btc;
 		start = date.find_first_not_of(" \t");
 		end = date.find_last_not_of(" \t");
 		if (start != std::string::npos && end != std::string::npos)
@@ -89,6 +88,6 @@ void BitcoinExchange::exchanging(const std::string &file)
 	m = fileParser(f);
 	if (m.empty())
 		throw std::runtime_error("memory failure");
-	(f.close(), f.open(file), thrower(f));
+	(f.close(), f.open(file.c_str()), thrower(f));
 	finders(m, f);
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 09:55:23 by htrindad          #+#    #+#             */
-/*   Updated: 2026/04/16 19:26:59 by htrindad         ###   ########.fr       */
+/*   Updated: 2026/04/26 20:02:56 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 int main(int ac, char **av)
 {
+	if (ac != 2)
+		return (std::cerr << "Need an argument\n", -1);
 	try
 	{
 		BitcoinExchange::exchanging(av[1]);
 	}
 	catch (std::exception &e)
 	{
-		std::cerr << "Impossible to comply, due to " << e.what() << "\n\n 'data.csv' non-existent, or failure to open the txt file.";
+		std::cerr << "Impossible to comply, due to " << e.what() << "\n\n 'data.csv' non-existent, or failure to open the txt file.\n";
 		return -1;
 	}
 	return 0;
