2c95d0ccd9b43725010ef57b099897f647824dbc
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2026-04-26 19:48:54 +0100

    attempt00

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 09:57:45 by htrindad          #+#    #+#             */
-/*   Updated: 2026/04/23 14:32:57 by htrindad         ###   ########.fr       */
+/*   Updated: 2026/04/26 19:42:59 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,21 @@ static inline std::map<std::string, float> fileParser(std::ifstream &file)
 	std::string			line;
 	std::string			date;
 	std::string			btc;
+	float				value;
 
 	std::getline(file, line); // skip "data,exchange_rate"
 	while (std::getline(file, line))
 	{
-		ss = line;
+		ss << line;
 		std::getline(ss, date, ',');
 		std::getline(ss, btc);
-		m.insert(std::make_pair(date, std::stof(btc)));
+		std::ifstringstream(btc) >> value;
+		m.insert(std::make_pair(date, value));
 	}
 	return m;
 }
 
-static inline void thrower(const std::ifstream &stream)
+static inline void thrower(std::ifstream &stream)
 {
 	if (stream.fail())
 		throw InvalidFile();
@@ -50,6 +52,8 @@ static inline void finders(const std::map<std::string, float> &m, const std::ifs
 	std::string				line;
 	std::string				date;
 	std::size_t				fo;
+	std::size_t				start;
+	std::size_t				end;
 	float					btc;
 
 	it = m.begin();
@@ -57,12 +61,21 @@ static inline void finders(const std::map<std::string, float> &m, const std::ifs
 	while (std::getline(f, line))
 	{
 		fo = line.find_first_of("|");
-		date = line.subtr(fo);
-		btc = std::stof();
+		date = line.substr(0, fo);
+		std::ifstringstream(line.substr(fo + 1)) >> btc;
+		start = date.find_first_not_of(" \t");
+		end = date.find_last_not_of(" \t");
+		if (start != std::string::npos && end != std::string::npos)
+			date = date.substr(start, end - start + 1);
 		it = m.lower_bound(date);
-		if (it->first != date)
-			it--;
-		std::cout << date << " => " << 
+		if (it == m.end() || it->first != date)
+		{
+			if (it == m.begin())
+				throw std::range_error("Error with the date found");
+			else
+				it--;
+		}
+		std::cout << date << " => " << btc << " = " << (btc * it->second) << '\n';
 	}
 }
 
