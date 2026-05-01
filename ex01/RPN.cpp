cce3d200adab00fd1d605884fa736773843afaae
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2026-05-01 06:17:39 +0100

    RPN

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,8 +6,70 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/01 04:09:57 by htrindad          #+#    #+#             */
-/*   Updated: 2026/05/01 04:10:08 by htrindad         ###   ########.fr       */
+/*   Updated: 2026/05/01 06:16:10 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
+
+//CDO
+RPN::RPN() { std::cout << "Nope\n"; }
+RPN::~RPN() { std::cout << "Nuh uh\n"; }
+RPN::RPN(const RPN &ref)
+{
+	(void)ref;
+	std::cout << "Not even close\n";
+}
+RPN &RPN::operator=(const RPN &ref)
+{
+	(void)ref;
+	std::cout << "shut up...";
+	return *this;
+}
+
+// The one and only
+
+void RPN::solver(char const *av)
+{
+	std::istringstream	ss(av);
+	std::string		str;
+	std::list<int>		l;
+	int			a;
+	int			b;
+
+	while (ss >> str)
+	{
+		if (std::isdigit(str[0]))
+			l.push_back(std::atoi(str.c_str()));
+		else
+		{
+			if (l.size() < 2)
+				throw std::runtime_error("Error");
+			b = l.back(); l.pop_back();
+			a = l.back(); l.pop_back();
+			switch(str[0])
+			{
+				case '+':
+					l.push_back(a + b);
+					break ;
+				case '-':
+					l.push_back(a - b);
+					break ;
+				case '*':
+					l.push_back(a * b);
+					break ;
+				case '/':
+					if (!b)
+						throw std::runtime_error("Error: dividing by zero");
+					l.push_back(a / b);
+					break ;
+				default:
+					throw std::runtime_error("Error: Bad formating");
+			}
+		}
+	}
+	if (l.size() > 1)
+		throw std::runtime_error("Error: Bad formating");
+	std::cout << l.back() << '\n';
+	l.pop_back();
+}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,31 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/01 03:55:06 by htrindad          #+#    #+#             */
-/*   Updated: 2026/05/01 06:15:39 by htrindad         ###   ########.fr       */
+/*   Created: 2026/05/01 05:18:19 by htrindad          #+#    #+#             */
+/*   Updated: 2026/05/01 06:05:44 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
-#include <iostream>
-#include <list>
-#include <string>
-#include <cctype>
-#include <stdexcept>
-#include <cstdlib>
-#include <sstream>
+#include "RPN.hpp"
 
-class RPN
+int main(int ac, char **av)
 {
-	private:
-	public:
-		RPN();
-		~RPN();
-		RPN(const RPN &ref);
-		RPN &operator=(const RPN &ref);
-		static void solver(char const *av);
-};
+	if (ac != 2)
+		return (std::cerr << "Not the right ammount of args\n", -1);
+	try
+	{
+		RPN::solver(av[1]);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << "Something went wrong\n";
+		return -1;
+	}
+	return 0;
+}
